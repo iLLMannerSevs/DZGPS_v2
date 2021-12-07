2fecd5873772a65647c914121232f42b4f62dda5
iLLMannerSevs <54834910+iLLMannerSevs@users.noreply.github.com>
2021-12-07 11:59:56 -0800

    init gps dir

---
+++ gps/maps/cherno.js
@@ -0,0 +1,10 @@
+﻿InitMap({
+    CRS: MGRS_CRS(15360, 15360, 256),
+    tilePattern: '/maps/cherno/{z}/{x}/{y}.jpg',
+    maxZoom: 6,
+    minZoom: 0,
+    defaultZoom: 2,
+    attribution: '&copy; Bohemia Interactive',
+    tileSize: 256,
+    center: [7500, 7500]
+});
